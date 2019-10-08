9e0d78ab6111fa2e98765831e0b1a7efd8003e7f
bfitz22 <bffitzgerald89@gmail.com>
2019-10-08 04:09:35 -0400

    complete tutorial

---
+++ app/javascript/packs/dashboard.jsx
@@ -0,0 +1,54 @@
+// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
+// of the page.
+
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import $ from 'jquery';
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: '',
+      autoCompleteResults: [],
+      itemsSelected: {},
+      showItemSelected: false
+    };
+
+    $.getJSON('/search?q=' + this.state.term).then(response => this.setState({ autoCompleteResults: response.items }))
+  }
+
+  getAutoCompleteResults(e) {
+    this.setState({
+      term: e.target.value 
+    }, () => {
+      $.getJSON('/search?q=' + this.state.term).then(response => this.setState({ autoCompleteResults: response.items }))
+    });
+  }
+
+  render() {
+    let autoCompleteList = this.state.autoCompleteResults.map((response, index) => {
+      return <div key={index}>
+        <h2>{response.title}</h2>
+        <p>{response.description}</p>
+      </div>
+    });
+
+    return (
+      <div>
+        <input ref={ (input) => { this.searchBar = input } } value={ this.state.term } 
+        onChange={ this.getAutoCompleteResults.bind(this) } type='text' placeholder='Search...' />
+        { autoCompleteList }
+      </div>
+    )
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Dashboard />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
